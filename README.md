aa539966056ff96dfebf8ac69178ca46fa085d49
isaacwerneck <isaacwerneck60@gmail.com>
2026-06-12 02:49:24 -0300

    primeira versão

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # osmenordograjau.com-biblioteca
+
+biblioteca dos menó, contendo várias frases e arquivos importantes da trajetória desses menor.
+
+
+## color pallet
+
+- background
---
+++ script.js
@@ -0,0 +1,6 @@
+// os menor do grajaú
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('%cOS MENOR DO GRAJAÚ', 'font-size: 18px; color: #cc0000; font-family: monospace;');
+    console.log('%cFEITO COM SANGUE E SUOR', 'font-size: 12px; color: #666; font-family: monospace;');
+});
